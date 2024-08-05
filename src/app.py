6b327916eaea05a02ace8e2c4d6d15a969d70cfa
objektwerks <objektwerks@runbox.com>
2024-08-05 10:53:23 -0400

    enhanced app.py

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
-import datetime
+from datetime import datetime
 
 greeting: str = 'Greetings, Pythonistas!'
 print(greeting)
 
-date = datetime.date
-print(date)
+today = datetime.now
+
+print(today.strftime("%x"))
+print(today.strftime("%X"))
