e2484155486fa5082eb53c7f3449d730f2360c50
objektwerks <objektwerks@runbox.com>
2024-08-05 13:27:00 -0400

    enhanced basics.py

--- src/basics.py
+++ src/basics.py
@@ -5,6 +5,10 @@ age: int = 27
 person: str = f'f-string: {name} is {age}.'
 print(person)
 
+print(f'str cast: {str(3)}')
+print(f'int cast: {int(3)}')
+print(f'float cast: {float(3)}')
+
 print(f'array[0] = {name[0]}')
 print(f'len: {len(person)}')
 print(f'slice: {person[10:15]}')
