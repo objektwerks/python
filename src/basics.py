27057423e4c1634b0820ff1c101f8e8760cc4f40
objektwerks <objektwerks@runbox.com>
2024-08-05 12:50:41 -0400

    enhanced basics.py

--- src/basics.py
+++ src/basics.py
@@ -1,8 +1,15 @@
 # basics
 
 multilineComment: str = """
-    This is a
-    multiline
-    comment.
+    This is a multiline
+    comment that fails
+    to format correctly.
 """.strip()
 print(multilineComment)
+print()
+
+name: str = 'Fred Flintstone'
+age = 27
+person = f"{name} is {age}."
+print(person)
+print()
