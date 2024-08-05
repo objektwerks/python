56558ded6f135cf39bbbe8da195dc94ab1cc23d9
objektwerks <objektwerks@runbox.com>
2024-08-05 11:25:05 -0400

    edited app.py

--- src/app.py
+++ src/app.py
@@ -2,10 +2,12 @@ from datetime import datetime
 
 greeting: str = 'Greetings, Pythonistas!'
 print(greeting)
+print()
 
 today: datetime = datetime.today()
 print('Date: ' + today.strftime('%Y-%m-%d'))
 print('Time: ' + today.strftime('%H:%M:%S'))
 
+print()
 closing: str = 'Code on!'
 print(closing)
