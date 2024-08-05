5fdbdb2f8bb834e682b26ccd2581475013e40842
objektwerks <objektwerks@runbox.com>
2024-08-05 15:11:55 -0400

    enhanced collections.py

--- src/collections.py
+++ src/collections.py
@@ -5,5 +5,6 @@ print(f'list: {ints}')
 print(f'len: {len(ints)}')
 print(f'type: {type(ints)}')
 
-bag: list = ['a', 1, 'b', 2, 'c', 3]
+bag: list[str | int] = ['a', 1, 'b', 2, 'c', 3]
 print(f'bag: {bag}')
+print(f'type: {type(bag)}')
