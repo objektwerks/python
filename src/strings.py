bdd2a1bea1d39b8c035cc469a97f63fcb1ba699c
objektwerks <objektwerks@runbox.com>
2024-08-05 13:05:30 -0400

    enhanced strings.py

--- src/strings.py
+++ src/strings.py
@@ -1,16 +1,17 @@
 # strings
 
 multilineComment: str = """
-    multiline comment:
-    that fails to format
-    correctly.
+  multiline comment:
+  that fails to format
+  correctly.
 """.strip()
 print(multilineComment)
 
 name: str = 'Fred Flintstone'
-age = 27
-person = f'f-string: {name} is {age}.'
-print(person)
+age: int = 27
+person: str = f'f-string: {name} is {age}.'
+for p in person:
+  print(p)
 
 print(f'len: {len(person)}')
 print(f'slice: {person[10:15]}')
