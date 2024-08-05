5b13196157d95536f336c09962d3be6acbe698c0
objektwerks <objektwerks@runbox.com>
2024-08-05 13:07:09 -0400

    enhanced strings.py

--- src/strings.py
+++ src/strings.py
@@ -10,8 +10,10 @@ print(multilineComment)
 name: str = 'Fred Flintstone'
 age: int = 27
 person: str = f'f-string: {name} is {age}.'
-for p in person:
-  print(p)
+print(person)
 
 print(f'len: {len(person)}')
 print(f'slice: {person[10:15]}')
+
+for c in "abc":
+  print(c)
