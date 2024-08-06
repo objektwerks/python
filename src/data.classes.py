754a93dcce7ee92d204d44770f6bac2feb374592
objektwerks <objektwerks@runbox.com>
2024-08-06 11:35:47 -0400

    enhanced data.classes.py

--- src/data.classes.py
+++ src/data.classes.py
@@ -3,7 +3,8 @@ from dataclasses import dataclass
 
 @dataclass
 class Car:
+    make: str
     model: str
     year: int
 
-print(f'car: {Car('porsche', 2024)}')
+print(f'car: {Car('porsche', '911', 2024)}')
