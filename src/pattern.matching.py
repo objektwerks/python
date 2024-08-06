a932e95ecd2ee8eba4893281a5fb2fe3fd05a91a
objektwerks <objektwerks@runbox.com>
2024-08-06 16:12:51 -0400

    enhanced pattern.matching.py

--- src/pattern.matching.py
+++ src/pattern.matching.py
@@ -1,15 +1,24 @@
 # pattern matching
 
-def matchOn(i: int) -> str:
+def matchOnInt(i: int) -> str:
   match i:
-      case 0: return 'zero'
-      case 1: return 'one'
-      case 2: return 'two'
-      case 3: return 'three'
-      case _: return 'unknown'
+    case 0: return 'zero'
+    case 1: return 'one'
+    case 2: return 'two'
+    case 3: return 'three'
+    case _: return 'unknown'
   
-print(f'match on 0 to {matchOn(0)}')
-print(f'match on 1 to {matchOn(1)}')
-print(f'match on 2 to {matchOn(2)}')
-print(f'match on 3 to {matchOn(3)}')
-print(f'match on 4 to {matchOn(4)}')
+print(f'match on 0 to {matchOnInt(0)}')
+print(f'match on 1 to {matchOnInt(1)}')
+print(f'match on 2 to {matchOnInt(2)}')
+print(f'match on 3 to {matchOnInt(3)}')
+print(f'match on 4 to {matchOnInt(4)}')
+
+def matchOnList(ints: list[int]) -> str:
+  match ints:
+    case [first, *rest]: return f'first: {first}, rest: {rest}'
+    case _: return 'unknown'
+
+
+ints: list[int] = [1, 2, 3]
+print(f'match on {ints} to {matchOnList(ints)}')
