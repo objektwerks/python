bc4dbf565bc9222c3e9fe30a6ff3504bb4b21e35
objektwerks <objektwerks@runbox.com>
2024-08-06 13:54:02 -0400

    enhanced data.classes.py

--- src/data.classes.py
+++ src/data.classes.py
@@ -10,7 +10,11 @@ class Car:
 print(f'car: {Car('porsche', '911', 2024)}')
 
 @dataclass(frozen=True)
-class Position:
+class Pos:
     name: str
     lon: float = 0.0
     lat: float = 0.0
+
+pos_one = Pos("a", 1.0, 1.0)
+pos_two = Pos('b', 1.0, 1.0)
+pos_three = Pos('c', 3.0, 3.0)
