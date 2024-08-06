2367e8843d17803090670e2574f148369ee90a55
objektwerks <objektwerks@runbox.com>
2024-08-06 16:36:23 -0400

    enhanced pattern.matching.py

--- src/pattern.matching.py
+++ src/pattern.matching.py
@@ -27,14 +27,13 @@ print(f'match on {ints} to {matchOnList(ints)}')
 @dataclass(frozen=True)
 class Circle:
   radius: float = 0.0
-
-  def diameter(self) -> float:
-    return self.radius * 2
   
 def matchOnCircle(circle: Circle) -> str:
   match circle:
+    case Circle(radius) if radius < 0.0: return f'a diameter of 0.0'
     case Circle(radius): return f'a diameter of {radius * 2}'
     case _: return 'unknown'
 
 circle = Circle(1.5)
-print(f'match on {circle} to {matchOnCircle(circle)}')
+print(f'match on {Circle(1.5)} to {matchOnCircle(circle)}')
+print(f'match on {Circle()} to {matchOnCircle(Circle())}')
