8bc6af5c047fe9d04feff5b4b84c43f9e9c5ac72
objektwerks <objektwerks@runbox.com>
2024-08-06 16:05:32 -0400

    enhanced pattern.matching.py

--- src/pattern.matching.py
+++ src/pattern.matching.py
@@ -1,1 +1,11 @@
 # pattern matching
+
+def matchOn(i: int) -> str:
+  match i:
+      case 0: return 'zero'
+      case 1: return 'one'
+      case 2: return 'two'
+      case 3: return 'three'
+      case _: return 'unknown'
+  
+print(f'match on 0 to {matchOn(0)}')
