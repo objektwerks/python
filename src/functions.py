f7ddb9c56230f985a48dc6e6411740355584c020
objektwerks <objektwerks@runbox.com>
2024-08-06 09:41:24 -0400

    enhanced functions.py

--- src/functions.py
+++ src/functions.py
@@ -6,7 +6,7 @@ def add(ints: list[int]) -> int:
     acc += i
   return acc
 
-def square(i: int) -> int:
+def square(i: int = 1) -> int:
   return i * i
 
 numbers: list[int] = [1, 2, 3]
@@ -14,3 +14,4 @@ print(f'{numbers} sum equals {add(numbers)}')
 
 two: int = 2
 print(f'{two} squared equals {square(two)}')
+print(f'default arg squared equals {square()}')
