cb8017f358c06f415307dfc477b49da8423e1f53
objektwerks <objektwerks@runbox.com>
2024-08-06 10:07:57 -0400

    enhanced lambdas.py

--- src/lambdas.py
+++ src/lambdas.py
@@ -1,6 +1,12 @@
 # lambdas
 from typing import Callable
 
+add: Callable[[int, int], int] = lambda x, y: x + y
+
+one: int = 1
+two: int = 2
+print(f'{one} plus {two} equals {add(one, two)}')
+
 square: Callable[[int], int] = lambda i: i * i
 
-print(f'sqrt of 2 equals {square(2)}')
+print(f'square root of 2 equals {square(2)}')
