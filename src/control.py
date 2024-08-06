babee7582b6528baa0f14bf128a3c7050fdfbca5
objektwerks <objektwerks@runbox.com>
2024-08-06 09:00:18 -0400

    enhanced control.py

--- src/control.py
+++ src/control.py
@@ -9,3 +9,10 @@ elif two > one:
     print(f'{two} is greater than {one}')
 else:
     print(f'{two} and {one} must be equal')
+
+i: int = 1
+while i < 4:
+  print(i)
+  i += 1
+else:
+  print(f"{i} is no longer less than {i} ")
