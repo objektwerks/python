1ccc7eaf6e3f2856ea2508510791440a112415fb
objektwerks <objektwerks@runbox.com>
2024-08-06 08:56:29 -0400

    enhanced control.py

--- src/control.py
+++ src/control.py
@@ -5,7 +5,7 @@ two: int = 2
 
 if one > two:
     print(f'{one} is greater than {two}')
-elif two < one:
-    print(f'{two} is less than {one}')
-else:
+elif two > one:
     print(f'{two} is greater than {one}')
+else:
+    print(f'{two} and {one} must be equal')
