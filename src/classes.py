f77d6ac39ea8691f4a596446fa20494a14bc1c30
objektwerks <objektwerks@runbox.com>
2024-08-06 10:32:17 -0400

    enhanced classes.py

--- src/classes.py
+++ src/classes.py
@@ -5,3 +5,10 @@ class Simple:
 
 print(f'simple class: {Simple()}')
 print(f'simple class id: {Simple().id}')
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+print(f'person: {Person('Fred Flintstone', 27)}')
