97a9e13b527b7f55bfea7d0c9f0cd9abf3e247ef
objektwerks <objektwerks@runbox.com>
2024-08-06 10:57:13 -0400

    enhanced classes.py

--- src/classes.py
+++ src/classes.py
@@ -28,8 +28,8 @@ class Student(Person):
   def __str__(self):
     return f'[Student] name: {self.name} age: {self.age} school: {self.school}'
 
-  def speak(self):
-    print(f'{self.name} speaking as student ...')
+  def speak(self) -> str:
+    return f'{self.name} speaking as student ...'
 
 print(f'person: {Student('Barney Rubble', 27, 'Rockhurst')}')
 print(f'person: {Student('Barney Rubble', 27, 'Rockhurst').speak()}')
