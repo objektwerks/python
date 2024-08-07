b9ac895e32d1ce5d2dda5ce389a54bccbe3c938c
objektwerks <objektwerks@runbox.com>
2024-08-07 15:30:17 -0400

    enhanced lambdas.py

--- src/lambdas.py
+++ src/lambdas.py
@@ -11,3 +11,6 @@ print(f'{one} plus {two} equals {add(one, two)}')
 square: Callable[[int], int] = lambda i: i * i
 
 print(f'square root of 2 equals {square(2)}')
+
+multiply = lambda x: lambda y: x * y
+print(f'partial to {multiply(2)(3)}')
