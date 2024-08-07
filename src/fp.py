edef07942d80b30bb1e6ff1fd3ea80bc06bd1561
objektwerks <objektwerks@runbox.com>
2024-08-07 10:37:17 -0400

    edited fp.py

--- src/fp.py
+++ src/fp.py
@@ -1,15 +1,18 @@
 # functional programming - fp
 
+from functools import reduce
 from typing import Callable
 
-square: Callable[[int], int] = lambda i: i * i
-
-even: Callable[[int], bool] = lambda i: i % 2 == 0
-
 ints: list[int] = [1, 2, 3]
+
+square: Callable[[int], int] = lambda i: i * i
 
 squares = map(square, ints)
 print(f'map(square,{ints}) to {list(squares)}')
 
+even: Callable[[int], bool] = lambda i: i % 2 == 0
+
 evens = filter(even, ints)
 print(f'filter(even,{ints}) to {list(evens)}')
+
+add: Callable[[int, int], int] = lambda x, y: x + y
