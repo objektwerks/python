3b1f44fb8470805ae299ef1173e27d0b5ab16890
objektwerks <objektwerks@runbox.com>
2024-08-07 15:41:37 -0400

    fromatted dataclasses.py

--- src/data.classes.py
+++ src/data.classes.py
@@ -4,17 +4,17 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Car:
-    make: str
-    model: str
-    year: int
+  make: str
+  model: str
+  year: int
 
 print(f'car: {Car('porsche', '911', 2024)}')
 
 @dataclass(frozen=True)
 class Pos:
-    name: str
-    lon: float = 0.0
-    lat: float = 0.0
+  name: str
+  lon: float = 0.0
+  lat: float = 0.0
 
 pos_one = Pos("a", 1.0, 1.0)
 pos_two = Pos('a', 1.0, 1.0)
