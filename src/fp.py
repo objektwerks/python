3098e8d1c9ea8f8f722ac0c4372885adc97d261b
objektwerks <objektwerks@runbox.com>
2024-08-07 10:24:33 -0400

    enhanced fp.py

--- src/fp.py
+++ src/fp.py
@@ -6,6 +6,7 @@ square: Callable[[int], int] = lambda i: i * i
 
 even: Callable[[int], bool] = lambda i: i % 2 == 0
 
+odd: Callable[[int], bool] = lambda i: i % 2 != 0
 
 ints: list[int] = [1, 2, 3]
 
