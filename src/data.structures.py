8806ff61a5416f704492042fd94f6416d54d091e
objektwerks <objektwerks@runbox.com>
2024-08-07 12:24:38 -0400

    enhanced data.structures.py

--- src/data.structures.py
+++ src/data.structures.py
@@ -1,6 +1,7 @@
 # data structures
 
 from array import array
+from collections import namedtuple
 
 ints: list[int] = [1, 2, 3]
 print(f'list: {ints}')
@@ -13,8 +14,15 @@ print(f'len: {len(bag)}')
 print(f'type: {type(bag)}')
 
 strings: tuple[str, str, str] = ('a', 'b', 'c')
-print(f'tuple: {tuple}')
+print(f'tuple: {strings}')
 print(f'type: {type(strings)}')
+
+point: tuple[float, float] = namedtuple('point', 'x y')
+point1 = point(1.0, 2.0)
+point2 = point(3.0, 4.0)
+print(f'point 1: {point1.x}/{point1.y}')
+print(f'point 2: {point2.x}/{point2.y}')
+print(f'type: {type(point)}')
 
 floats: set[float] = {1.1, 2.2, 3.3, 1.1, 2.2, 3.3}
 print(f'set: {floats}')
