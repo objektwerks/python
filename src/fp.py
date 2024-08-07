2c3937f1af5af88cc9c4fb6f2082e906b13f5eb1
objektwerks <objektwerks@runbox.com>
2024-08-07 10:39:38 -0400

    enhanced fp.py

--- src/fp.py
+++ src/fp.py
@@ -7,12 +7,15 @@ ints: list[int] = [1, 2, 3]
 
 square: Callable[[int], int] = lambda i: i * i
 
-squares = map(square, ints)
+squares: map = map(square, ints)
 print(f'map(square,{ints}) to {list(squares)}')
 
 even: Callable[[int], bool] = lambda i: i % 2 == 0
 
-evens = filter(even, ints)
+evens: filter = filter(even, ints)
 print(f'filter(even,{ints}) to {list(evens)}')
 
 add: Callable[[int, int], int] = lambda x, y: x + y
+
+total: int = reduce(add, ints)
+print(f'reduce {ints} to {total}')
