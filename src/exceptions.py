053e7eb7ccc40e75a7b9b1cc48b0f32fe737289d
objektwerks <objektwerks@runbox.com>
2024-08-07 09:14:48 -0400

    enhanced exceptions.py

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,2 +1,13 @@
 # exceptions
 
+def openFile(file: str, options: str) -> str:
+  with open('data.txt', 'r') as f:
+    return f.read()
+
+data: str = ''
+try:
+  data = openFile('./requirements.txt', 'r')
+except:
+  data = 'file not found'
+
+print(f'opened requirements.txt file: {data}')
