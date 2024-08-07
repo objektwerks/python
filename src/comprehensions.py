252484558f436b10ea8d4674e196e0d6be1d8815
objektwerks <objektwerks@runbox.com>
2024-08-07 08:09:27 -0400

    enhanced comprehensions

--- src/comprehensions.py
+++ src/comprehensions.py
@@ -5,6 +5,8 @@ ints: list[int] = [1, 2, 3]
 squares: list[int] = [i * i for i in ints]
 print(f'ints {ints} squared {squares}')
 
-evens: list[int] = [i for i in ints if i / 2 == 0]
+evens: list[int] = [i for i in ints if i % 2 == 0]
 print(f'even ints {evens}')
 
+odds: list[int] = [i for i in ints if i % 2 != 0]
+print(f'odd ints {odds}')
