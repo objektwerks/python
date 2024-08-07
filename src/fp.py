78658f8b271f853b898f88a0fe0b08fccdbff632
objektwerks <objektwerks@runbox.com>
2024-08-07 10:32:47 -0400

    edited fp.py

--- src/fp.py
+++ src/fp.py
@@ -6,8 +6,6 @@ square: Callable[[int], int] = lambda i: i * i
 
 even: Callable[[int], bool] = lambda i: i % 2 == 0
 
-odd: Callable[[int], bool] = lambda i: i % 2 != 0
-
 ints: list[int] = [1, 2, 3]
 
 squares = map(square, ints)
@@ -15,5 +13,3 @@ print(f'map(square,{ints}) to {list(squares)}')
 
 evens = filter(even, ints)
 print(f'filter(even,{ints}) to {list(evens)}')
-
-odds = filter(odd, ints)
