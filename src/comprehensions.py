8b63413005d4204f645c16a245d5e7eb9de4acba
objektwerks <objektwerks@runbox.com>
2024-08-07 08:15:39 -0400

    enhanced comprehensions

--- src/comprehensions.py
+++ src/comprehensions.py
@@ -10,3 +10,7 @@ print(f'even ints {evens}')
 
 odds: list[int] = [i for i in ints if i % 2 != 0]
 print(f'odd ints {odds}')
+
+quote: str = "life, uh, finds a way"
+vowels: set[str] = {char for char in quote if char in "aeiou"}
+print(f'from {quote} vowels filtered and deduped to {vowels}')
