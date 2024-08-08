7ca1ffa153c1955c234fa09e0f932eb5cea29237
objektwerks <objektwerks@runbox.com>
2024-08-08 03:06:55 -0400

    enhanced expression.py

--- src/expressions.py
+++ src/expressions.py
@@ -1,9 +1,19 @@
 # expression - functional programming library
 
-from expression import curry
+from expression import curry, pipe
+from typing import Callable
 
 @curry(1)
 def add(x: int, y: int) -> int:
   return x + y
 
 print(f'curry function to {add(1)(2)}')
+
+value: int = 1
+function1: Callable[[int], int] = lambda i: i + 1
+function2: Callable[[int], int] = lambda i: i * 2
+
+piped = pipe(value, function1, function2)
+composed = function2(function1(value))
+                     
+assert piped == composed
