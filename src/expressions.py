2de2a55f26800b6ef9d6909ec3b486b1dca75c8a
objektwerks <objektwerks@runbox.com>
2024-08-08 03:12:39 -0400

    enhanced expressions.py

--- src/expressions.py
+++ src/expressions.py
@@ -16,6 +16,7 @@ function2: Callable[[int], int] = lambda i: i * 2
 piped = pipe(value, function1, function2)
 composed = function2(function1(value))
 
+print(f'function1: {function1(value)}')
 print(f'piped: {piped}')
 print(f'composed: {composed}')
 print(f'piped vs composed equal: {piped == composed}')
