95a668ddc32314614738e077e6be8607c40e36a4
objektwerks <objektwerks@runbox.com>
2024-08-08 03:41:30 -0400

    enhanced expressions.py

--- src/expressions.py
+++ src/expressions.py
@@ -1,6 +1,6 @@
 # expression - functional programming library
 
-from expression import curry, pipe
+from expression import curry, pipe, Some, Option
 from typing import Callable
 
 @curry(1)
@@ -22,3 +22,9 @@ print(f'function2: {function2(value)}')
 print(f'pipe(value, function1, function2): {piped}')
 print(f'function2(function1(value)): {composed}')
 print(f'piped vs composed equal: {piped == composed}')
+
+some: Option[int] = Some(1)
+function3 = lambda option: option.map(lambda i: i + 1)
+function4 = lambda option: option.map(lambda i: i + 2)
+
+print(f'option piped vs composed equal: {some.pipe(function3, function4) == function4(function3(some))}')
