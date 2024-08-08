a51a9d75d5492447baf609b104c0338f74b3d762
objektwerks <objektwerks@runbox.com>
2024-08-08 03:15:58 -0400

    enhanced expressions.py

--- src/expressions.py
+++ src/expressions.py
@@ -11,12 +11,13 @@ print(f'curry function to {add(1)(2)}')
 
 value: int = 1
 function1: Callable[[int], int] = lambda i: i + 1
-function2: Callable[[int], int] = lambda i: i * 2
+function2: Callable[[int], int] = lambda i: i + 2
 
 piped = pipe(value, function1, function2)
 composed = function2(function1(value))
 
 print(f'function1: {function1(value)}')
-print(f'piped: {piped}')
-print(f'composed: {composed}')
+print(f'function2: {function2(value)}')
+print(f'pipe(value, function1, function2): {piped}')
+print(f'function2(function1(value)): {composed}')
 print(f'piped vs composed equal: {piped == composed}')
