4ad0df06882c46892f0de973de28ac7c19760efe
objektwerks <objektwerks@runbox.com>
2024-08-15 09:47:17 -0400

    edited files.dirs

--- src/files.dirs.py
+++ src/files.dirs.py
@@ -4,18 +4,18 @@ import os
 
 with open('requirements.txt', 'r') as f:
     data = f.read()
-    print(f'read requirements.txt:\n\n{data}')
+    print(f'read: requirements.txt:\n\n{data}')
 
 with open('test.txt', 'w') as f:
     data = 'test data'
     f.write(data)
-    print('write text.txt\n')
+    print('write: text.txt\n')
 
 os.remove('test.txt')
-print('remove text.txt')
+print('remove: text.txt\n')
 
 rootpath: str = './'
-print("listdir ./")
+print("listdir: ./ \n")
 for entry in os.listdir(rootpath):
     if os.path.isfile(os.path.join(rootpath, entry)):
         print(entry)
