0f1ae1e9115a99f374974923c4c758e91b625adc
objektwerks <objektwerks@runbox.com>
2024-08-15 11:59:56 -0400

    enhanced exceptions.py

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,2 +1,8 @@
 # exceptions
 
+try:
+  with open('not.there.txt', 'r') as f:
+    data = f.read()
+    print(f'read: requirements.txt:\n\n{data}')
+except FileNotFoundError as error:
+  print(f'try / except: {error}')
