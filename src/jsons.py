eedbecc6341132fd954eb6550927938e7a5d2d25
objektwerks <objektwerks@runbox.com>
2024-08-15 11:32:57 -0400

    enhanced jsons.py

--- src/jsons.py
+++ src/jsons.py
@@ -6,3 +6,7 @@ letters: dict[str, str] = {'name': 'fred flintstone', 'gender': 'male'}
 lettersAsJson: str = json.dumps(letters)
 print(f'dumps: dict to json: {lettersAsJson}')
 print(f'loads: json to dict: {json.loads(lettersAsJson)}')
+
+with open('json.txt', 'w') as f:
+    f.write(lettersAsJson)
+    print('write: json.txt\n')
