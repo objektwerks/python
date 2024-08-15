52d430e7a2604f7306dc867ff313cdd8429d962b
objektwerks <objektwerks@runbox.com>
2024-08-15 09:49:34 -0400

    enhanced files.dirs

--- src/files.dirs.py
+++ src/files.dirs.py
@@ -11,12 +11,11 @@ with open('test.txt', 'w') as f:
     f.write(data)
     print('write: text.txt\n')
 
-os.remove('test.txt')
-print('remove: text.txt\n')
-
 rootpath: str = './'
 print("listdir: ./ \n")
 for entry in os.listdir(rootpath):
     if os.path.isfile(os.path.join(rootpath, entry)):
         print(entry)
 
+os.remove('test.txt')
+print('\nremove: text.txt\n')
