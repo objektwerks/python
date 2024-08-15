ddf62009ade3b2baf2761f7d5d05f91c4d06b28e
objektwerks <objektwerks@runbox.com>
2024-08-15 11:33:49 -0400

    enhanced jsons.py

--- src/jsons.py
+++ src/jsons.py
@@ -1,6 +1,7 @@
 # json
 
 import json
+import os
 
 letters: dict[str, str] = {'name': 'fred flintstone', 'gender': 'male'}
 lettersAsJson: str = json.dumps(letters)
@@ -10,3 +11,10 @@ print(f'loads: json to dict: {json.loads(lettersAsJson)}')
 with open('json.txt', 'w') as f:
     f.write(lettersAsJson)
     print('write: json.txt\n')
+
+rootpath: str = './'
+print("listdir: ./ \n")
+for entry in os.listdir(rootpath):
+    if os.path.isfile(os.path.join(rootpath, entry)):
+        print(entry)
+
