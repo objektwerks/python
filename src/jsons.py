0601bfe5abe2c7fda15cc35d56ac05ce9f4dc5cc
objektwerks <objektwerks@runbox.com>
2024-08-15 11:31:27 -0400

    enhanced jsons.py

--- src/jsons.py
+++ src/jsons.py
@@ -4,5 +4,5 @@ import json
 
 letters: dict[str, str] = {'name': 'fred flintstone', 'gender': 'male'}
 lettersAsJson: str = json.dumps(letters)
-print(f'dict to json: {lettersAsJson}')
-print(f'json to dict: {json.loads(lettersAsJson)}')
+print(f'dumps: dict to json: {lettersAsJson}')
+print(f'loads: json to dict: {json.loads(lettersAsJson)}')
