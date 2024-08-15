e652a400e7f3401e83c661c96362132511295de3
objektwerks <objektwerks@runbox.com>
2024-08-15 09:35:39 -0400

    enhanced files.dirs

--- src/files.dirs.py
+++ src/files.dirs.py
@@ -3,3 +3,9 @@
 with open('requirements.txt', 'r') as f:
     data = f.read()
     print(f'read requirements.txt:\n\n{data}')
+
+with open('test.txt', 'w') as f:
+    data = 'test data'
+    f.write(data)
+    print('write text.txt')
+
