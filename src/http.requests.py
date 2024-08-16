dc4f7d94d20aee407efc922edd6e78b977cff4dc
objektwerks <objektwerks@runbox.com>
2024-08-16 08:26:37 -0400

    enhanced http requests

--- src/http.requests.py
+++ src/http.requests.py
@@ -4,4 +4,5 @@ import requests
 from requests import Response
 
 response: Response = requests.get("https://randomuser.me/api/")
-print(f'response: {response.text}')
+print(f'response status code: {response.status_code}')
+print(f'response text: {response.text}')
