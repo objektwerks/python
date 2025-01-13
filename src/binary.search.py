ad2b32b98df7cb314b340906f2657627a48e1322
objektwerks <objektwerks@runbox.com>
2025-01-13 13:02:21 -0500

    edited binary search

--- src/binary.search.py
+++ src/binary.search.py
@@ -1,20 +1,20 @@
 # binary search
 
 def binary_search(ints: list[int], item: int) -> int:
-    low = 0
-    high = len(ints) - 1
+  low = 0
+  high = len(ints) - 1
 
-    while low <= high:
-        mid = (low + high) // 2
-        guess = ints[mid]
-        if guess == item:
-            return mid
-        elif guess > item:
-            high = mid - 1
-        else:
-            low = mid + 1
+  while low <= high:
+    mid = (low + high) // 2
+    guess = ints[mid]
+    if guess == item:
+        return mid
+    elif guess > item:
+        high = mid - 1
+    else:
+        low = mid + 1
 
-    return -1
+  return -1
 
 ints: list[int] = [1, 3, 5, 7, 9]
 
