727893fe2883504c059c2544fd098fcdca9b1be2
objektwerks <objektwerks@runbox.com>
2025-01-13 13:01:24 -0500

    enhanced binary search

--- src/binary.search.py
+++ src/binary.search.py
@@ -1,8 +1,6 @@
 # binary search
 
-from typing import List
-
-def binary_search(ints: List[int], item: int) -> int:
+def binary_search(ints: list[int], item: int) -> int:
     low = 0
     high = len(ints) - 1
 
@@ -18,7 +16,7 @@ def binary_search(ints: List[int], item: int) -> int:
 
     return -1
 
-ints: List[int] = [1, 3, 5, 7, 9]
+ints: list[int] = [1, 3, 5, 7, 9]
 
 print(f'binary search on ints {ints} for 3 equal index of: {binary_search(ints, 3)}')
 print(f'binary search on ints {ints} for 0 equal index of: {binary_search(ints, 0)}')
