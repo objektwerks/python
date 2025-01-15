f373d140a7d87e5f194c39cd11c8459bc97f4300
objektwerks <objektwerks@runbox.com>
2025-01-15 10:21:04 -0500

    added recursion.py

---
+++ src/recursion.py
@@ -0,0 +1,9 @@
+# recursion
+
+def factorial(n: int, acc: int = 1) -> int:
+  if n == 1:
+    return acc
+  else:
+    return factorial(n - 1, acc * n)
+
+print(f'factorial of 9 should equal 362_880 == {factorial(9)}')
