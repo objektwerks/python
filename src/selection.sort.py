bc2639c7c9fbc3b2e636f904bb84dc11e4107d10
objektwerks <objektwerks@runbox.com>
2025-01-15 09:54:30 -0500

    enhanced selection.sort.py

--- src/selection.sort.py
+++ src/selection.sort.py
@@ -1,3 +1,12 @@
 # selection sort
-#
-#
+
+def findSmallest(arr):
+  smallest = arr[0]
+  smallest_index = 0
+
+  for i in range(1, len(arr)):
+    if arr[i] < smallest: smallest = arr[i] smallest_index = i
+    Stores the smallest value
+    Stores the index of the smallest value
+
+  return smallest_index
