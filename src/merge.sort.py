8bd6d8c26eb0f222d7d5c1bc5c8e8bb5f7528dc1
objektwerks <objektwerks@runbox.com>
2025-01-17 14:18:15 -0500

    enhanced merge sort

--- src/merge.sort.py
+++ src/merge.sort.py
@@ -2,9 +2,9 @@
 
 def mergesort(items: list[int]):
   if len(items) > 1:
-    mid = len(items) // 2
-    left_half = items[:mid]
-    right_half = items[mid:]
+    itemsLen = len(items)
+    left_half = items[:itemsLen]
+    right_half = items[itemsLen:]
 
     mergesort(left_half)
     mergesort(right_half)
@@ -12,20 +12,20 @@ def mergesort(items: list[int]):
     i = j = k = 0
 
     while i < len(left_half) and j < len(right_half):
-        if left_half[i] < right_half[j]:
-            items[k] = left_half[i]
-            i += 1
-        else:
-            items[k] = right_half[j]
-            j += 1
-        k += 1
+      if left_half[i] < right_half[j]:
+          items[k] = left_half[i]
+          i += 1
+      else:
+          items[k] = right_half[j]
+          j += 1
+      k += 1
 
     while i < len(left_half):
-        items[k] = left_half[i]
-        i += 1
-        k += 1
+      items[k] = left_half[i]
+      i += 1
+      k += 1
 
     while j < len(right_half):
-        items[k] = right_half[j]
-        j += 1
-        k += 1
+      items[k] = right_half[j]
+      j += 1
+      k += 1
