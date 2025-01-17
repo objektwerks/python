166c9ddf13fe6e7b91cd95bf15d88baf3fa05be7
objektwerks <objektwerks@runbox.com>
2025-01-17 14:42:29 -0500

    enhanced merge sort

--- src/merge.sort.py
+++ src/merge.sort.py
@@ -34,4 +34,4 @@ def mergesort(items: list[int]) -> None:
 
 ints: list[int] = [10, 5, 2, 3]
 
-print(f'merge sort of ints {ints} sorts to: {mergesort(ints)}')
+print(f'merge sort of ints {ints} sorts in-place: {mergesort(ints)} to: {ints}')
