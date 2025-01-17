dd1d4dd30d32a56d558928976c5b3a4a10cc5a05
objektwerks <objektwerks@runbox.com>
2025-01-17 13:52:42 -0500

    enhanced quick sort

--- src/quick.sort.py
+++ src/quick.sort.py
@@ -1,12 +1,12 @@
 # quick sort
 
-def quicksort(array):
-  if len(array) < 2:
-    return array
+def quicksort(items: list[int]):
+  if len(items) < 2:
+    return items
   else:
-    pivot = array[0]
-    less = [i for i in array[1:] if i <= pivot]
-    greater = [i for i in array[1:] if i > pivot]
+    pivot = items[0]
+    less = [i for i in items[1:] if i <= pivot]
+    greater = [i for i in items[1:] if i > pivot]
     return quicksort(less) + [pivot] + quicksort(greater)
 
 print(quicksort([10, 5, 2, 3]))
