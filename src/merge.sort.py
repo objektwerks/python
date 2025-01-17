cd746c64567d4060664b21682df242c4fdb8309b
objektwerks <objektwerks@runbox.com>
2025-01-17 14:16:26 -0500

    enhanced merge sort

--- src/merge.sort.py
+++ src/merge.sort.py
@@ -1,1 +1,31 @@
 # merge sort
+
+def merge_sort(items: list[int]):
+  if len(items) > 1:
+    mid = len(items) // 2
+    left_half = items[:mid]
+    right_half = items[mid:]
+
+    merge_sort(left_half)
+    merge_sort(right_half)
+
+    i = j = k = 0
+
+    while i < len(left_half) and j < len(right_half):
+        if left_half[i] < right_half[j]:
+            items[k] = left_half[i]
+            i += 1
+        else:
+            items[k] = right_half[j]
+            j += 1
+        k += 1
+
+    while i < len(left_half):
+        items[k] = left_half[i]
+        i += 1
+        k += 1
+
+    while j < len(right_half):
+        items[k] = right_half[j]
+        j += 1
+        k += 1
