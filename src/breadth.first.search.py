8d874daeb0e38dce2d7ad67aa715bd6e8f4902db
objektwerks <objektwerks@runbox.com>
2025-01-18 15:28:23 -0500

    enhanced breadth.first.search.py

--- src/breadth.first.search.py
+++ src/breadth.first.search.py
@@ -1,1 +1,17 @@
 # breadth first search
+
+from collections import deque
+
+def breadthFirstSearch(map: dict[int, list[int]], start: int):
+  queue = deque([start])
+  visited = []
+
+  while queue:
+    node = queue.popleft()
+    visited.append(node)
+
+    for child in map[node]:
+      if child not in visited and child not in queue:
+        queue.append(child)
+
+  return visited
