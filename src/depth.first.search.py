6e588643683ac6ef71cadf2f31aa66998ae0b872
objektwerks <objektwerks@runbox.com>
2025-01-20 03:03:37 -0500

    enhanced depth.first.search.py

--- src/depth.first.search.py
+++ src/depth.first.search.py
@@ -1,1 +1,17 @@
 # depth first search
+
+import os
+
+def recurse(rootdir: str) -> list[str]:
+  files: list[str] = []
+  for file in sorted( os.listdir(rootdir) ):
+    path = os.path.join(rootdir, file)
+    if os.path.isfile(path):
+      files.append(file)
+    else:
+      recurse(path)
+  return files
+
+print("recurse:")
+for file in recurse("./src"):
+  print(file)
