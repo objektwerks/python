c57a558e7663e0a997660b9d5718d52fc087f538
objektwerks <objektwerks@runbox.com>
2025-01-20 05:26:47 -0500

    enhanced dijkstra.py

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -1,6 +1,7 @@
 # dijkstra's algo
 
 from dataclasses import dataclass
+import heapq
 from typing import Optional
 
 graph: dict[str, dict[str, int]] = {
@@ -11,10 +12,36 @@ graph: dict[str, dict[str, int]] = {
   'd': {}
 }
 
-@dataclass(frozen=True)
+@dataclass
 class Node:
   x: int = 0
   y: int = 0
   distance: float = float('inf')
   parent: Optional[str] = None
   finished: bool = False
+
+def dijkstra(graph: dict[str, dict[str, float]],
+             source: str) -> tuple[dict[str, float], dict[str, str]]:
+    nodes: dict[str, Node] = {node: Node(0, 0) for node in graph}
+    nodes[source].distance = 0
+    queue: list[tuple[float, str]] = [ (0, source) ]
+
+    while queue:
+      distance, node = heapq.heappop(queue)
+      if nodes[node].finished:
+        continue
+      nodes[node].finished = True
+
+      for neighbor, weight in graph[node].items():
+        if nodes[neighbor].finished:
+          continue
+        newDistance = distance + weight
+        if newDistance < nodes[neighbor].distance:
+          nodes[neighbor].distance = newDistance
+          nodes[neighbor].parent = node
+          heapq.heappush(queue, (newDistance, neighbor))
+
+    shortest_path: dict[str, float] = {node: nodes[node].distance for node in nodes}
+    previous_nodes: dict[str, str] = {node: nodes[node].parent for node in nodes}
+
+    return shortest_path, previous_nodes
