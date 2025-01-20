466d969532e9173707447d0ff389117a7de6f690
objektwerks <objektwerks@runbox.com>
2025-01-20 05:49:16 -0500

    enhanced dijkstra.py

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -21,28 +21,28 @@ class Node:
   finished: bool = False
 
 def findShortestPaths(graph: dict[str, dict[str, float]], source: str) -> dict[str, float]:
-    nodes: dict[str, Node] = {node: Node(0, 0) for node in graph}
-    nodes[source].distance = 0
-    queue: list[tuple[float, str]] = [ (0, source) ]
+  nodes: dict[str, Node] = {node: Node(0, 0) for node in graph}
+  nodes[source].distance = 0
+  queue: list[tuple[float, str]] = [ (0, source) ]
 
-    while queue:
-      distance, node = heapq.heappop(queue)
-      if nodes[node].finished:
+  while queue:
+    distance, node = heapq.heappop(queue)
+    if nodes[node].finished:
+      continue
+    nodes[node].finished = True
+
+    for neighbor, weight in graph[node].items():
+      if nodes[neighbor].finished:
         continue
-      nodes[node].finished = True
+      newDistance = distance + weight
+      if newDistance < nodes[neighbor].distance:
+        nodes[neighbor].distance = newDistance
+        nodes[neighbor].parent = node
+        heapq.heappush(queue, (newDistance, neighbor))
 
-      for neighbor, weight in graph[node].items():
-        if nodes[neighbor].finished:
-          continue
-        newDistance = distance + weight
-        if newDistance < nodes[neighbor].distance:
-          nodes[neighbor].distance = newDistance
-          nodes[neighbor].parent = node
-          heapq.heappush(queue, (newDistance, neighbor))
+  shortestPaths: dict[str, float] = {node: nodes[node].distance for node in nodes}
 
-    shortestPaths: dict[str, float] = {node: nodes[node].distance for node in nodes}
-
-    return shortestPaths
+  return shortestPaths
 
 print(f'graph {graph}')
 print(f'find shortest paths: {findShortestPaths(graph, "s")}')
