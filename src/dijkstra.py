49a7f007d0a0779811236e4d03c1cad12ec4068d
objektwerks <objektwerks@runbox.com>
2025-01-20 05:11:54 -0500

    enhanced dijkstra.py

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -1,7 +1,7 @@
 # dijkstra's algo
 
 from dataclasses import dataclass
-import heapq
+from typing import Optional
 
 graph: dict[str, dict[str, int]] = {
   's': {'a':8, 'b':4},
@@ -16,31 +16,5 @@ class Node:
   x: int = 0
   y: int = 0
   distance: float = float('inf')
-  parent = None
+  parent: Optional[str] = None
   finished: bool = False
-
-def dijkstra(graph: dict[str, dict[str, int]], source: Node):
-  nodes = {}
-  for node in graph:
-    nodes[node] = Node()
-  nodes[source].distance = 0
-
-  queue: list[tuple[int, Node]] = [ (0, source) ]
-
-  while queue:
-      distance = heapq.heappop(queue)
-      node = heapq.heappop(queue)
-
-      if nodes[node].finished:
-          continue
-      nodes[node].finished=True
-      for neighbor in graph[node]:
-          if nodes[neighbor].finished:
-              continue
-          newDistance = distance + graph[node][neighbor]
-          if newDistance<nodes[neighbor].d:
-              nodes[neighbor].d=newDistance
-              nodes[neighbor].parent=node
-              heapq.heappush(queue,(newDistance,neighbor))
-
-  return nodes
