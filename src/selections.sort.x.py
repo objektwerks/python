a027be0dc059ab149f9da068dfbc131d09635f6d
objektwerks <objektwerks@runbox.com>
2025-01-21 06:59:00 -0500

    edited selection sort x

--- src/selections.sort.x.py
+++ src/selections.sort.x.py
@@ -1,14 +1,14 @@
 # selections sort x - O(n^2)
 
 
-def selectionSort(items):
+def selectionSort(items: list[int]) -> list[int]:
   for i in range(len(items) - 1):
-              lowest_number_index = i
+    lowestNumberIndex = i
 
-  for j in range(i + 1, len(items)):
-    if items[j] < items[lowest_number_index]:
-      lowest_number_index = j
-    if lowest_number_index != i:
-      items[i], items[lowest_number_index] = items[lowest_number_index], items[i]
+    for j in range(i + 1, len(items)):
+      if items[j] < items[lowestNumberIndex]:
+        lowestNumberIndex = j
+      if lowestNumberIndex != i:
+        items[i], items[lowestNumberIndex] = items[lowestNumberIndex], items[i]
     
   return items
