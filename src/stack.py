66a65b2367a312de9b8f26753bf4e8d61bbe1a62
objektwerks <objektwerks@runbox.com>
2025-01-21 13:02:07 -0500

    enhanced stack

--- src/stack.py
+++ src/stack.py
@@ -7,14 +7,18 @@ class Stack:
   def push(self, element) -> None:
     self.data.append(element)
 
-  def pop(self) -> int | None:
+  def pop(self) -> int:
     if len(self.data) > 0:
       return self.data.pop()
     else:
-      return None
+      return int('-inf')
     
 ints: Stack = Stack()
 ints.push(1)
 ints.push(2)
 ints.push(3)
-print(f'stack push [1, 2, 3]: {ints}')
+print(f'stack push on [1, 2, 3]: {ints}')
+ints.pop()
+ints.pop()
+ints.pop()
+print(f'stack pop on [1, 2, 3]: {ints}')
