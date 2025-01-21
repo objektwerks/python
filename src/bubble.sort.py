2c985b99154b8e0ee4ab9ebfba6b1d261db1e65a
objektwerks <objektwerks@runbox.com>
2025-01-21 05:15:51 -0500

    edit

--- src/bubble.sort.py
+++ src/bubble.sort.py
@@ -1,16 +1,16 @@
 # bubble sort
 
 def bubblesort(items: list[int]) -> list[int]:
-  unsortedIndex: int = len(items) - 1
+  lastUnsortedIndex: int = len(items) - 1
   sorted: bool = False
 
   while not sorted:
     sorted = True
-    for i in range(unsortedIndex):
+    for i in range(lastUnsortedIndex):
       if items[i] > items[i + 1]:
         items[i], items[i + 1] = items[i + 1], items[i]
         sorted = False
-    unsortedIndex -= 1
+    lastUnsortedIndex -= 1
 
   return items
 
