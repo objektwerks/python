cf1bb414b2e73a031dc9d2743e9d408989bd692f
objektwerks <objektwerks@runbox.com>
2025-01-21 04:52:02 -0500

    format

--- src/files.dirs.py
+++ src/files.dirs.py
@@ -4,19 +4,19 @@ from collections import deque
 import os
 
 with open('requirements.txt', 'r') as f:
-    data = f.read()
-    print(f'read: requirements.txt:\n\n{data}')
+  data = f.read()
+  print(f'read: requirements.txt:\n\n{data}')
 
 with open('test.txt', 'w') as f:
-    data = 'test data'
-    f.write(data)
-    print('write: text.txt\n')
+  data = 'test data'
+  f.write(data)
+  print('write: text.txt\n')
 
 rootpath: str = './'
 print("listdir: ./ \n")
 for entry in os.listdir(rootpath):
-    if os.path.isfile(os.path.join(rootpath, entry)):
-        print(entry)
+  if os.path.isfile(os.path.join(rootpath, entry)):
+      print(entry)
 
 os.remove('test.txt')
 print('\nremove: text.txt\n')
