036aeaab2ad5c87d2c12ae5b8b3b9d61632e5904
objektwerks <objektwerks@runbox.com>
2025-01-21 04:38:44 -0500

    implemented bubble sort

--- src/bubble.sort.py
+++ src/bubble.sort.py
@@ -1,2 +1,13 @@
 # bubble sort
 
+def bubble_sort(array):
+  unsorted_until_index = len(array) - 1
+  sorted = False
+  while not sorted:
+    sorted = True
+    for i in range(unsorted_until_index):
+      if array[i] > array[i+1]:
+        array[i], array[i+1] = array[i+1], array[i]
+        sorted = False
+    unsorted_until_index -= 1
+  return array
