106131d3b0911f755622f4abd52ceb5a404623cd
objektwerks <objektwerks@runbox.com>
2025-01-21 05:49:39 -0500

    enhanced binary search

--- src/binary.search.py
+++ src/binary.search.py
@@ -4,20 +4,20 @@ def indexof(items: list[int], item: int) -> int:
   lowIndex: int = 0
   highIndex: int = len(items) - 1
 
-  result: int = -1
+  targetItemIndex: int = -1
 
   while lowIndex <= highIndex:
     midIndex: int = (lowIndex + highIndex) // 2
     midIndexItem: int = items[midIndex]
     if midIndexItem == item:
-      result = midIndex
+      targetItemIndex = midIndex
       break
     elif midIndexItem > item:
       highIndex = midIndex - 1
     else:
       lowIndex = midIndex + 1
 
-  return result
+  return targetItemIndex
 
 ints: list[int] = [1, 3, 5, 7, 9]
 print(f'binary search on ints {ints} for indexof 3 returns an indexof: {indexof(ints, 3)}')
