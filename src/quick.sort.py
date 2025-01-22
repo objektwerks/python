c8a27795815d7ea01ee849f577af3a7eb2e8341f
objektwerks <objektwerks@runbox.com>
2025-01-22 13:05:09 -0500

    edit

--- src/quick.sort.py
+++ src/quick.sort.py
@@ -1,13 +1,13 @@
 # quick sort - O(log n)
 
-def quickSort(items: list[int]) -> list[int]:
+def sort(items: list[int]) -> list[int]:
   if len(items) < 2:
     return items
   else:
     pivotItem: int = items[0]
     lesserItems: list[int] = [i for i in items[1:] if i <= pivotItem]
     greaterItems: list[int] = [i for i in items[1:] if i > pivotItem]
-    return quickSort(lesserItems) + [pivotItem] + quickSort(greaterItems)
+    return sort(lesserItems) + [pivotItem] + sort(greaterItems)
 
 ints: list[int] = [10, 5, 2, 3]
-print(f'quick sort of ints {ints} sorts to: {quickSort(ints)}')
+print(f'quick sort of ints {ints} sorts to: {sort(ints)}')
