6fc848e92c544f2cd513d7ccd88cbecaf8c6420e
objektwerks <objektwerks@runbox.com>
2025-01-24 18:18:52 -0500

    enhanced tree

--- src/tree.py
+++ src/tree.py
@@ -6,12 +6,9 @@ from typing import Protocol, TypeVar
 T = TypeVar("T")
 
 @dataclass
-class Node(Protocol[T]):
+class TreeNode(Protocol[T]):
   data: T
-
-@dataclass
-class Tree(Node[T]):
-  left: Node[T]
-  righ: Node[T]
+  left: T
+  right: T
 
 # TODO - not sure this approach really works in Python ( 2025.1.24 )
