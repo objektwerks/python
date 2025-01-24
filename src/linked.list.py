2e9e61471d03378aaac8a2a57c301fd410ab94bd
objektwerks <objektwerks@runbox.com>
2025-01-24 05:44:43 -0500

    enhanced linked list

--- src/linked.list.py
+++ src/linked.list.py
@@ -11,17 +11,6 @@ class Node(Protocol[T]):
   next: T
 
 @dataclass
-class LinkedList(Protocol[T]):
-  head: T
-  tail: list[T] = list[T]()
-
-  def of(self, items: list[T]) -> None:
-    len: int = len(items)
-    if len > 0:
-      head = items[0]
-      if len > 1:
-        tail = list[T][1:len - 1]
-    else:
-      raise Exception("LinkedList requires at least 1 item.")
-
-data: LinkedList[int] = LinkedList(1, [2, 3])
+class LinkedList(Node[T]):
+  head: Node[T]
+  tail: list[Node[T]]
