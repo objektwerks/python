d04a3c1b26536813ce7bb3eaa1a0289f9fb46dce
objektwerks <objektwerks@runbox.com>
2025-01-26 09:16:57 -0500

    enhanced validation

--- src/validation.py
+++ src/validation.py
@@ -2,7 +2,7 @@
 
 from datetime import date
 from enum import Enum
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 
 class Gender(Enum):
   male = 1
@@ -13,6 +13,15 @@ class Person(BaseModel):
   born: date = Field(frozen = True)
   gender: Gender = Field(frozen = True)
   married: bool = Field(frozen = True)
+
+  @field_validator("born")
+  @classmethod
+  def validateBorn(cls, born: date) -> date:
+    today = date.today()
+    eighteen = date(today.year - 18, today.month, today.day)
+    if born > eighteen:
+      raise ValueError("Married couples should be 18 years or older.")
+    return born
 
 person: Person = Person(
   name = "Fred Flintstone",
