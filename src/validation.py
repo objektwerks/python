604e0f1842264f86e8f7dff0947b4a3b4241d007
objektwerks <objektwerks@runbox.com>
2025-01-26 08:23:04 -0500

    enhanced validation

--- src/validation.py
+++ src/validation.py
@@ -1,9 +1,15 @@
 # pydantic
 
+from enum import Enum
 from pydantic import BaseModel
+
+class Gender(Enum):
+  male = 1
+  female = 2
 
 class Person(BaseModel):
   name: str
   age: int
+  gender: Gender
 
-print(f'pydantic class: {Person(name = "Fred Flintstone", age = 24)}')
+print(f'pydantic class: {Person(name = "Fred Flintstone", age = 24, gender = Gender.male)}')
