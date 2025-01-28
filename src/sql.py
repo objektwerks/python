db7342e6b56043f6ce9cd6e21f1149c81bc0936d
objektwerks <objektwerks@runbox.com>
2025-01-28 02:35:08 -0500

    enhanced sql

--- src/sql.py
+++ src/sql.py
@@ -7,11 +7,11 @@ ddl = '''
   create table todo (
     id integer primary key auto_increment,
     task text not null,
-    created text not null,
-    completed text not null
+    created timestamp not null,
+    completed timestamp not null
   );
 '''
 
-with sql.connect(":memory:") as connection:
+with sql.connect(":memory:", detect_types = sql.PARSE_DECLTYPES | sql.PARSE_COLNAMES) as connection:
   cursor = connection.cursor()
 
