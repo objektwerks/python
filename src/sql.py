1055be2c39a188d3417eb7c6f3a37fca76f7f61e
objektwerks <objektwerks@runbox.com>
2025-01-28 02:31:36 -0500

    enhanced sql

--- src/sql.py
+++ src/sql.py
@@ -1,6 +1,16 @@
 # sqlite3
 
+from datetime import date
 import sqlite3 as sql
+
+ddl = '''
+  create table todo (
+    id integer primary key auto_increment,
+    task text not null,
+    created text not null,
+    completed text not null
+  );
+'''
 
 with sql.connect(":memory:") as connection:
   cursor = connection.cursor()
