c33fe8d373079423015dcafb6b2d50ae95b4d5b7
objektwerks <objektwerks@runbox.com>
2025-01-28 16:12:39 -0500

    corrected unit test error

--- src/recursion.test.py
+++ src/recursion.test.py
@@ -2,7 +2,11 @@
 
 from recursion import factorial
 from unittest import TestCase
+import unittest
 
 class RecursionTest(TestCase):
   def testFactorial(self):
-    self.assertEqual( factorial(9), 362_880, "factorial(9) != 362_880" )
+    self.assertEqual( factorial(9), 362880, "factorial(9) != 362_880" )
+
+if __name__ == "__main__":
+  unittest.main()
