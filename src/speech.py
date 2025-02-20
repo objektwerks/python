fe10aff3074aba07b4f8723e4ab69c9d7d1a1c7d
objektwerks <objektwerks@runbox.com>
2025-02-20 06:19:24 -0500

    edited speech.py

--- src/speech.py
+++ src/speech.py
@@ -5,7 +5,7 @@ import pyttsx3 as tts
 engine = tts.init()
 
 # Rate of speaking.
-print(f'rate of speaking is: {engine.getProperty('rate')}')
+print(f'rate of speaking is set at: {engine.getProperty('rate')}')
 engine.setProperty('rate', 125)
 print(f'rate of speaking has been adjusted to: {engine.getProperty('rate')}')
 
