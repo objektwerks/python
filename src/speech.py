de6a2e8bb146a83c0a02d31d2f1ade65cdf168d1
objektwerks <objektwerks@runbox.com>
2025-02-20 06:17:13 -0500

    enhanced speech.py

--- src/speech.py
+++ src/speech.py
@@ -5,10 +5,9 @@ import pyttsx3 as tts
 engine = tts.init()
 
 # Rate of speaking.
-rate = engine.getProperty('rate')
-print(f'rate of speaking is: {rate}')
+print(f'rate of speaking is: {engine.getProperty('rate')}')
 engine.setProperty('rate', 125)
-print(f'rate of speaking has been adjusted to: {rate}')
+print(f'rate of speaking has been adjusted to: {engine.getProperty('rate')}')
 
 engine.say("These words will be converted from text to speech.")
 
