2b5125736aad369000f416aad5e36610d2d30c71
objektwerks <objektwerks@runbox.com>
2025-02-22 14:41:38 -0500

    enhanced http requrests

--- src/http.requests.py
+++ src/http.requests.py
@@ -3,7 +3,7 @@
 import requests
 from requests import Response
 
-response: Response = requests.get("https://randomuser.me/api/")
+response: Response = requests.get("https://api.chucknorris.io/jokes/random")
 print(f'response status code: {response.status_code}')
 print(f'response reason: {response.reason}')
 print(f'response headers:\n {response.headers}')
