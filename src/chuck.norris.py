92f401d2b1c58389ddb556c4d70f33b409b63419
objektwerks <objektwerks@runbox.com>
2025-02-22 14:07:55 -0500

    enhanced chuck norris

--- src/chuck.norris.py
+++ src/chuck.norris.py
@@ -4,12 +4,14 @@ import json
 import requests
 from requests import Response
 import pyttsx3 as tts
+from pyttsx3 import Engine
 
 api: str = "https://api.chucknorris.io/jokes/random"
 responseAsJson: str = requests.get(api).text
 joke: str = json.loads(responseAsJson)['value']
 print(f'joke:\n {joke}')
 
-engine = tts.init()
+engine: Engine = tts.init()
 engine.setProperty('rate', 125)
 engine.setProperty('volume', 0.75)
+engine.say(joke)
